34e4fb28986ef97f6758fd798ecc9a265b75c07e
Murillo77 <murillofsantanna@gmail.com>
2024-10-02 09:08:48 -0300

    Imagem de erro adicionada á página.

--- cp5-front-end/src/app/not-found.tsx
+++ cp5-front-end/src/app/not-found.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
+import ImagemErro from "../assets/erro_404_.jpg";
 
 export default function NotFound() {
     return (
         <div>
             <h1>404 - NotFound</h1>
             <h2>This page could not be found.</h2>
-            <Image layout="fill" src="" alt="Página de erro." />
-            <Image layout="intrinsic" src="" alt="Página de erro." width={400} height={400} />
+            <Image layout="intrinsic" src={ImagemErro} alt="Página de erro." width={400} height={400} />
         </div>
     )
 }
